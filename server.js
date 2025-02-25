863d633bcc0bf8f6c4b80c39fd0f83094594c619
노수민 <nosumin29@gmail.com>
2025-02-25 10:39:33 +0900

    chore: CORS 설정 추가

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// CORS 설정 추가
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Vercel 앱 도메인으로 제한하는 것이 좋음
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 server.listen(3000, () => {
